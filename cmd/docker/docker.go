db20d5af8a08c03b67dbcec6fc7c9d30686bf05c
lisa.li <lisa.x.li@sap.com>
2019-12-14 16:55:44 +0800

    docker cmd

--- cmd/docker/docker.go
+++ cmd/docker/docker.go
@@ -1,26 +1,22 @@
+// https://github.com/docker/cli/blob/v19.03.5/cmd/docker/docker.go
 package main
 
 import (
-	"github.com/docker/docker/cli/command"
-	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-	cliflags "github.com/docker/docker/cli/flags"
+	"github.com/docker/cli/cli/command"
 	"fmt"
-	"github.com/docker/docker/dockerversion"
-	"github.com/docker/docker/cliconfig"
-	"github.com/docker/docker/utils"
-	"github.com/docker/docker/cli"
-	"github.com/docker/docker/api/types/versions"
 	"os"
-	"github.com/Sirupsen/logrus"
-	"github.com/docker/docker/pkg/term"
-	"github.com/docker/docker/cli/command/commands"
-	"errors"
+	cliflags "github.com/docker/cli/cli/flags"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/docker/cli/cli"
+	"github.com/spf13/pflag"
 )
 
-func newDockerCommand(dockerCli *command.DockerCli) *cobra.Command {
-	opts := cliflags.NewClientOptions()
-	var flags *pflag.FlagSet
+func newDockerCommand(dockerCli *command.DockerCli) *cli.TopLevelCommand{
+	var (
+		opts    *cliflags.ClientOptions
+		flags   *pflag.FlagSet
+	)
 
 	cmd := &cobra.Command{
 		Use:              "docker [OPTIONS] COMMAND [ARG...]",
@@ -28,152 +24,43 @@ func newDockerCommand(dockerCli *command.DockerCli) *cobra.Command {
 		SilenceUsage:     true,
 		SilenceErrors:    true,
 		TraverseChildren: true,
-		Args:             noArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if opts.Version {
-				showVersion()
-				return nil
+			if len(args) == 0 {
+				return command.ShowHelp(dockerCli.Err())(cmd, args)
 			}
-			return dockerCli.ShowHelp(cmd, args)
+			return fmt.Errorf("docker: '%s' is not a docker command.\nSee 'docker --help'", args[0])
+
 		},
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			// daemon command is special, we redirect directly to another binary
-			if cmd.Name() == "daemon" {
-				return nil
-			}
-			// flags must be the top-level command flags, not cmd.Flags()
-			opts.Common.SetDefaultOptions(flags)
-			dockerPreRun(opts)
-			if err := dockerCli.Initialize(opts); err != nil {
-				return err
-			}
-			return isSupported(cmd, dockerCli.Client().ClientVersion(), dockerCli.HasExperimental())
-		},
+		DisableFlagsInUseLine: true,
 	}
-	cli.SetupRootCommand(cmd)
 
-	cmd.SetHelpFunc(func(ccmd *cobra.Command, args []string) {
-		if dockerCli.Client() == nil { // when using --help, PersistenPreRun is not called, so initialization is needed.
-			// flags must be the top-level command flags, not cmd.Flags()
-			opts.Common.SetDefaultOptions(flags)
-			dockerPreRun(opts)
-			dockerCli.Initialize(opts)
-		}
-
-		if err := isSupported(ccmd, dockerCli.Client().ClientVersion(), dockerCli.HasExperimental()); err != nil {
-			ccmd.Println(err)
-			return
-		}
-
-		//hideUnsupportedFeatures(ccmd, dockerCli.Client().ClientVersion(), dockerCli.HasExperimental())
-
-		if err := ccmd.Help(); err != nil {
-			ccmd.Println(err)
-		}
-	})
-
-	flags = cmd.Flags()
-	flags.BoolVarP(&opts.Version, "version", "v", false, "Print version information and quit")
-	flags.StringVar(&opts.ConfigDir, "config", cliconfig.ConfigDir(), "Location of client config files")
-	opts.Common.InstallFlags(flags)
-
-	cmd.SetOutput(dockerCli.Out())
-	cmd.AddCommand(newDaemonCommand())
-	commands.AddCommands(cmd, dockerCli)
-
-	return cmd
+	opts, flags, _ = cli.SetupRootCommand(cmd)
+	flags.BoolP("version", "v", false, "Print version information and quit")
+	return cli.NewTopLevelCommand(cmd, dockerCli, opts, flags)
 }
 
-func noArgs(cmd *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		return nil
+
+func runDocker(dockerCli *command.DockerCli) error {
+	tcmd := newDockerCommand(dockerCli)
+
+	cmd, _, err := tcmd.HandleGlobalFlags()
+	if err != nil {
+		return err
 	}
-	return fmt.Errorf(
-		"docker: '%s' is not a docker command.\nSee 'docker --help'", args[0])
+
+	if err := tcmd.Initialize(); err != nil {
+		return err
+	}
+
+	return cmd.Execute()
 }
 
 func main() {
-	// Set terminal emulation based on platform as required.
-	stdin, stdout, stderr := term.StdStreams()
-	logrus.SetOutput(stderr)
-
-	dockerCli := command.NewDockerCli(stdin, stdout, stderr)
-	cmd := newDockerCommand(dockerCli)
-
-	if err := cmd.Execute(); err != nil {
-		if sterr, ok := err.(cli.StatusError); ok {
-			if sterr.Status != "" {
-				fmt.Fprintln(stderr, sterr.Status)
-			}
-			// StatusError should only be used for errors, and all errors should
-			// have a non-zero exit status, so never exit with 0
-			if sterr.StatusCode == 0 {
-				os.Exit(1)
-			}
-			os.Exit(sterr.StatusCode)
-		}
-		fmt.Fprintln(stderr, err)
+	dockerCli, err := command.NewDockerCli()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func showVersion() {
-	fmt.Printf("Docker version %s, build %s\n", dockerversion.Version, dockerversion.GitCommit)
-}
-
-func dockerPreRun(opts *cliflags.ClientOptions) {
-	cliflags.SetLogLevel(opts.Common.LogLevel)
-
-	if opts.ConfigDir != "" {
-		cliconfig.SetConfigDir(opts.ConfigDir)
-	}
-
-	if opts.Common.Debug {
-		utils.EnableDebug()
-	}
-}
-
-func hideUnsupportedFeatures(cmd *cobra.Command, clientVersion string, hasExperimental bool) {
-	cmd.Flags().VisitAll(func(f *pflag.Flag) {
-		// hide experimental flags
-		if !hasExperimental {
-			if _, ok := f.Annotations["experimental"]; ok {
-				f.Hidden = true
-			}
-		}
-
-		// hide flags not supported by the server
-		if flagVersion, ok := f.Annotations["version"]; ok && len(flagVersion) == 1 && versions.LessThan(clientVersion, flagVersion[0]) {
-			f.Hidden = true
-		}
-
-	})
-
-	//for _, subcmd := range cmd.Commands() {
-	//	// hide experimental subcommands
-	//	if !hasExperimental {
-	//		if _, ok := subcmd.Tags["experimental"]; ok {
-	//			subcmd.Hidden = true
-	//		}
-	//	}
-	//
-	//	// hide subcommands not supported by the server
-	//	if subcmdVersion, ok := subcmd.Tags["version"]; ok && versions.LessThan(clientVersion, subcmdVersion) {
-	//		subcmd.Hidden = true
-	//	}
-	//}
-}
-
-func isSupported(cmd *cobra.Command, clientVersion string, hasExperimental bool) error {
-	if !hasExperimental {
-		if _, ok := cmd.Tags["experimental"]; ok {
-			return errors.New("only supported with experimental daemon")
-		}
-	}
-
-	if cmdVersion, ok := cmd.Tags["version"]; ok && versions.LessThan(clientVersion, cmdVersion) {
-		return fmt.Errorf("only supported with daemon version >= %s", cmdVersion)
-	}
-
-	return nil
+	logrus.SetOutput(dockerCli.Err())
+	runDocker(dockerCli)
 }
--- go.mod
+++ go.mod
@@ -3,35 +3,49 @@ module play_docker
 go 1.13
 
 require (
-	github.com/Microsoft/go-winio v0.4.12 // indirect
-	github.com/Sirupsen/logrus v0.11.5 // indirect
+	github.com/Microsoft/hcsshim v0.8.7 // indirect
 	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412 // indirect
-	github.com/docker/distribution v2.6.2+incompatible // indirect
-	github.com/docker/docker v1.13.1 // indirect
+	github.com/containerd/cgroups v0.0.0-20191206154412-fada802a7909 // indirect
+	github.com/containerd/containerd v1.3.0-beta.2.0.20190828155532-0293cbd26c69
+	github.com/containerd/continuity v0.0.0-20191127005431-f65d91d395eb // indirect
+	github.com/containerd/fifo v0.0.0-20191213151349-ff969a566b00 // indirect
+	github.com/containerd/ttrpc v0.0.0-20191028202541-4f1b8fe65a5c // indirect
+	github.com/containerd/typeurl v0.0.0-20190911142611-5eb25027c9fd // indirect
+	github.com/docker/cli v0.0.0-20191212191748-ebca1413117a // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.4.2-0.20191213113251-3452f136aa68 // indirect
 	github.com/docker/docker-credential-helpers v0.6.3 // indirect
 	github.com/docker/go v1.5.1-1 // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
 	github.com/docker/go-metrics v0.0.1 // indirect
-	github.com/docker/go-units v0.4.0 // indirect
 	github.com/docker/libnetwork v0.5.6 // indirect
 	github.com/docker/libtrust v0.0.0-20160708172513-aabc10ec26b7 // indirect
 	github.com/docker/notary v0.4.2 // indirect
 	github.com/flynn-archive/go-shlex v0.0.0-20150515145356-3f9db97f8568 // indirect
+	github.com/gogo/googleapis v1.3.1 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
 	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/mattn/go-shellwords v1.0.6 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/opencontainers/runc v0.1.1 // indirect
 	github.com/opencontainers/runtime-spec v1.0.1 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
 	github.com/spf13/cobra v0.0.5 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/theupdateframework/notary v0.4.2 // indirect
+	github.com/syndtr/gocapability v0.0.0-20180916011248-d98352740cb2 // indirect
+	github.com/theupdateframework/notary v0.6.1 // indirect
 	github.com/vbatts/tar-split v0.11.1 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	golang.org/x/net v0.0.0-20191116160921-f9c825593386 // indirect
-	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
+	go.opencensus.io v0.22.2 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	google.golang.org/genproto v0.0.0-20191206224255-0243a4be9c8f // indirect
 	google.golang.org/grpc v1.25.1 // indirect
+	vbom.ml/util v0.0.0-20180919145318-efcd4e0f9787 // indirect
 )
